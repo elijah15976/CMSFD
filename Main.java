7e3ad813537e32aa50835a1edf853e4a14f0f755
Elijah15976 <eren5836@baysidehighschool.org>
2025-02-24 11:54:42 -0500

    init function

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+//Java Libraries
+import java.io.File;
+
+//Error Libraries
+import java.io.IOException;
+
 public class Main{
+  private static Logger lg = new Logger();
+
   public static void main(String[] args) {
-    
+    try{
+      init();
+    }
+    catch(IOException e){
+      lg.dispMessage(e.getStackTrace(), "warn");
+      return;
+    }
+  }
+
+  public static void init() throws IOException{
+    File logStuff = new File("./log/log.txt");
+    File templateFolder = new File("./template");
+
+    if(!logStuff.exists()){
+      logStuff.mkdir();
+    }
+    if(!templateFolder.exists()){
+      templateFolder.mkdir();
+    }
+
+    logStuff.createNewFile();
   }
 }
